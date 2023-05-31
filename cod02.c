5862bea05acd0aee31b238dfefc943ee1700f4b4
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-05-31 14:57:41 -0300

    cod02.c

---
+++ cod02.c
@@ -0,0 +1,32 @@
+/*Programa onde o usuário digite dois numeros e o programa fara a contagem, o usuário 
+também pode escolher o valor do salto entre os numeros*/
+
+#include <stdio.h>
+
+int main()
+{
+  float valorInicial, valorFinal, incremento, i;
+  printf("Digite o valor do valor inicial: ");
+  scanf("%f", &valorInicial);
+  printf("Digite o valor do valor final: ");
+  scanf("%f", &valorFinal);
+  printf("Digite o valor do incremento: ");
+  scanf("%f", &incremento);
+  i = valorInicial;
+  if(valorInicial < valorFinal){
+    printf(" O valor inicial e menor que o valor final. Portanto a contagem sera crescente!\n");
+   
+  }  
+  while(i <= valorFinal && incremento > 0){
+    printf("%.2f \n", i);
+    i += incremento;
+    }
+  if(valorFinal < valorInicial){
+    printf(" O valor final e menor que o valor final. Portanto a contagem sera decrescente!\n");
+  }
+  while(i >= valorFinal && incremento > 0){
+    printf("%.2f \n", i);
+    i -= incremento;
+  }
+  return 0;
+}
