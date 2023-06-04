33e1859a360d4e19649da16d4f3ed2e0a8a0c180
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-06-04 20:18:19 -0300

    co

--- cod06.c
+++ cod06.c
@@ -4,15 +4,17 @@
 
 int main()
 {
-  float carboidratos, lipidios, proteinas, gramas;
-  printf("Digite o valor de carboidratos do alimento por 100 gramas: ");
+  float carboidratos, lipidios, proteinas, gramas, porcao;
+  printf("Digite o valor de carboidratos: ");
   scanf("%f", &carboidratos);
-  printf("Digite o valor dos lipidios do alimento por 100 gramas: ");
+  printf("Digite o valor dos lipidios: ");
   scanf("%f", &lipidios);
-  printf("Digite o valor das proteinas do alimento por 100 gramas: ");
+  printf("Digite o valor das proteina: ");
   scanf("%f", &proteinas);
-  printf("Digite o valor de gramas do alimeto: ");
+  printf("O valor digitado acima corresponde a uma porcao de quantos gramas? \n");
+  scanf("%f", &porcao);
+  printf("Digite o valor de gramas total do alimento: ");
   scanf("%f", &gramas);
-  printf("O valor em calorias do alimento e %.2f", (((carboidratos*4)+(proteinas*4)+(lipidios*9))*gramas)/100);
+  printf("O valor em calorias do alimento e %.2f", (((carboidratos*4)+(proteinas*4)+(lipidios*9))*gramas)/porcao);
   return 0;
 }
