ac04c67fae257ff7b66bf1e5fa789f25cc648be9
Rafael Tomaz <rafaeltomaz2005@gmail.com>
2023-06-09 14:32:57 -0300

    cod09.c

--- cod08.c
+++ cod08.c
@@ -3,3 +3,32 @@ a) Qual é a média de idade do grupo
 b) Quantas pessoas tem mais de 18 anos
 c) Quantas pessoas tem menos de 5 anos
 d) Qual foi a maior idade lida */
+
+#include <stdio.h>
+
+int main()
+{
+  int idadePessoas = 0, idade, idadeMaior18 = 0, idadeMenor5 = 0, maiorIdade = 0;
+  float  idadeMedia = 0, somaIdade = 0;
+  while(idadePessoas < 10){
+    printf("\n Digite a idade: ");
+    scanf("%d", &idade);
+    somaIdade += idade;
+    if(idade >= 18){
+      idadeMaior18++;
+    }
+    if(idade < 5){
+      idadeMenor5++;
+    }
+    if(idade > maiorIdade){
+      maiorIdade = idade;
+    }
+    idadePessoas++;
+  }
+  idadeMedia = somaIdade/10;
+  printf("\n A media da idade do grupo e %.2f", idadeMedia);
+  printf("\n Quantidade de pessoas com idade maior que 18: %d", idadeMaior18);
+  printf("\n Quantidade pessoas idade menor que 5: %d", idadeMenor5);
+  printf("\n A maior idade e %d", maiorIdade);
+  return 0;
+}
--- cod09.c
+++ cod09.c
@@ -1,2 +1,37 @@
 /*Crie um programa no qual o computador vai sortear um número entre 1 e 10 e o jogador vai ter 3
 tentativas para tentar acertar.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+  int numeroSorteado, tentativas = 0, tentativaUser, resposta;
+  srand(time(NULL));
+  numeroSorteado = rand()%11;
+  printf("Este e um jogo para tentar adivinhar o numero entre 0 e 10");
+  printf("\n Voce tem 3 tentativas");
+  while(tentativas < 3){
+    printf("\n Digite um numero: ");
+    scanf("%d", &tentativaUser);
+    if(numeroSorteado == tentativaUser){
+      printf("\n Parabens! Voce acertou!!!");
+      printf("\n Tente novamente sempre que quiser");
+      return 0;
+    }
+    if(numeroSorteado != tentativaUser && tentativas < 2){
+      printf("\n Voce errou! Este nao era o numero correto.");
+      printf("\n Tente novamente");
+    }
+     if(numeroSorteado != tentativaUser && tentativas == 1){
+      printf("\n Esta e sua ultima chance!");
+    }
+    if(numeroSorteado != tentativaUser && tentativas == 2){
+      printf("\n\n Que pena, voce nao conseguiu.");
+      printf("\n Tente novamente sempre que quiser!");
+    }
+    tentativas++;
+  }
+  return 0;
+}
